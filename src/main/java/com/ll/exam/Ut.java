1089fea21b3e69d02e8333ca7e894b59811e8486
JI-NAMEUN <skadms1720@gmail.com>
2022-08-02 13:25:40 +0900

    git clone 하기

---
+++ src/main/java/com/ll/exam/Ut.java
@@ -0,0 +1,38 @@
+package com.ll.exam;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Ut {
+    public static class cls {
+
+        public static Object newObj(Class cls, Object defaultValue) {
+            try {
+                return cls.getDeclaredConstructor().newInstance();
+            } catch (InstantiationException e) {
+                return defaultValue;
+            } catch (IllegalAccessException e) {
+                return defaultValue;
+            } catch (InvocationTargetException e) {
+                return defaultValue;
+            } catch (NoSuchMethodException e) {
+                return defaultValue;
+            }
+        }
+    }
+
+    ;
+
+    public static class str {
+
+        public static String decapitalize(String string) {
+            if (string == null || string.length() == 0) {
+                return string;
+            }
+
+            char c[] = string.toCharArray();
+            c[0] = Character.toLowerCase(c[0]);
+
+            return new String(c);
+        }
+    }
+}
